2f3045c9119b4d0af4376ba04c4036bf4e352616
Hung <duyhungbmt06@gmail.com>
2026-05-29 19:21:56 +0700

    Update build.sh

--- script/download/build.sh
+++ script/download/build.sh
@@ -1,14 +1,14 @@
-python -m source.run.preprocess_raw_data \
---dataset_name hotpotqa; \
-python -m source.run.generate_passage_embeddings \
---model_name_or_path facebook/contriever-msmarco \
---passages ./data/embed_ready_data/hotpotqa.tsv \
---output_dir ./data/database/contriever_msmarco/hotpotqa \
---shard_id 0 \
---num_shards 1 \
---per_gpu_batch_size 1024; \
-python -m source.run.build_index \
---output_dir ./data/database/contriever_msmarco/hotpotqa; \
+# python -m source.run.preprocess_raw_data \
+# --dataset_name hotpotqa; \
+# python -m source.run.generate_passage_embeddings \
+# --model_name_or_path facebook/contriever-msmarco \
+# --passages ./data/embed_ready_data/hotpotqa.tsv \
+# --output_dir ./data/database/contriever_msmarco/hotpotqa \
+# --shard_id 0 \
+# --num_shards 1 \
+# --per_gpu_batch_size 1024; \
+# python -m source.run.build_index \
+# --output_dir ./data/database/contriever_msmarco/hotpotqa; \
 
 
 python -m source.run.preprocess_raw_data \
